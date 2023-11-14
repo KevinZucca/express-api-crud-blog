cef7d83b672d0f8d1bc07ef6696b63722d2a4c86
KevinZucca <kev.94@live.it>
2023-11-14 16:31:18 +0100

    feat: form-data accessed

--- routers/posts.js
+++ routers/posts.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/posts");
+const multer = require("multer");
 
 router.get("/", postController.index);
-router.post("/", postController.store);
+router.post("/", multer().none(), postController.store);
 router.get("/create", postController.create);
 router.get("/:slug", postController.show);
 router.delete("/:slug", postController.destroy);
